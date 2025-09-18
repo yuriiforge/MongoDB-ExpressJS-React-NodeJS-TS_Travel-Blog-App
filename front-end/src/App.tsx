843cca13895c9f22f4380af97f610b74f8fda952
YuriiStepaniuk <yurii.stepaniuk.dev@gmail.com>
2025-09-18 14:05:10 +0300

    Configure redux store

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -6,6 +6,8 @@ import { ROUTES } from './routes';
 import Layout from './components/Layout';
 import { QueryClientProvider } from '@tanstack/react-query';
 import queryClient from './services/queryClient';
+import { Provider, useSelector } from 'react-redux';
+import { store, type RootState } from './store';
 
 const router = createBrowserRouter([
   {
@@ -19,9 +21,13 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
+  const isLoggedIn = useSelector((s: RootState) => s.auth.isLoggedIn);
+
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </QueryClientProvider>
   );
 };
